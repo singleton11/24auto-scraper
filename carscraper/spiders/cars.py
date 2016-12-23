fee571db4deed45ecf4f2fe6f87089ffc73fd6ec
Anton Prokhorov <betrayer11@gmail.com>
2016-12-23 23:07:03 +0700

    Parse main information about cars from front page

--- carscraper/spiders/cars.py
+++ carscraper/spiders/cars.py
@@ -3,8 +3,31 @@ import scrapy
 
 
 class CarsSpider(scrapy.Spider):
-    name = "cars"
-    start_urls = ['http://24auto.ru/']
+    """Spider of http://24auto.ru"""
+    name = 'cars'
+    start_urls = ['http://www.24auto.ru/board/cars/']
+
+    base_url = 'http://www.24auto.ru/'
 
     def parse(self, response):
-        pass
+        selector = response.css('.board_table__row::attr(href)')
+        # If page is list of cars, have to go to every car on page
+        if len(selector):
+            for link in selector:
+                yield scrapy.Request(
+                    url='{base_url}/{path}'.format(
+                        base_url=self.base_url,
+                        path=link.extract())
+                    ,
+                    callback=self.parse
+                )
+        else:
+            entity = {}
+
+            for i, block in enumerate(response.css('.card_table')):
+                headers = block.css('th::text').extract()
+                values = block.css('td::text').extract()
+
+                entity.update(dict(zip(headers, values)))
+
+            yield entity
